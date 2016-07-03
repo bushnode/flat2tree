2cac38ebd893fba54907f859278ab513eaa77997
Patric Mathews <magamos@yandex.ru>
2016-07-03 20:02:03 +0500

    first commit

--- index.js
+++ index.js
@@ -1,54 +1,17 @@
 'use strict';
 
 /*
-Не сказано, что id не может быть 0.
-В случае ошибочных входных данных не должно генерироваться исключение.
-Входные данные модифицируются, что не оговорено.
+Preferences before other unflatten packages:
+- In case of erroneous input data exception must not be generated;
+- Input data will not be modified.
  */
 
-let xxx = [1,2,3];
-
-console.log('XXX', xxx.push(5));
-
-const nodes = [
-    {id: 1, parentId: 0},
-    {id: 2, parentId: 0},
-    {id: 3, parentId: 1},
-    {id: 4, parentId: 1},
-    {id: 5, parentId: 2},
-    {id: 6, parentId: 4},
-    {id: 7, parentId: 5}
-];
-
-let map = {}, node, roots = [];
-for (let i = 0; i < nodes.length; i += 1) {
-    node = nodes[i];
-    node.children = [];
-    map[node.id] = i;
-    if (node.parentId != 0) {
-        nodes[map[node.parentId]].children.push(node);
-    } else {
-        roots.push(node);
+module.exports = (nodes) => {
+    let result = [], idx = {0: {children: result}};
+    for (let i = 0; i < nodes.length; i++) {
+        let node = nodes[i];
+        let parent = idx[node.parentId];
+        idx[node.id] = parent.children[parent.children.length] = Object.assign({children: []}, node);
     }
-}
-
-console.log('RES:', JSON.stringify(roots, null, "\t"));
-
-const nodes2 = [
-    {id: 1, parentId: 0},
-    {id: 2, parentId: 0},
-    {id: 3, parentId: 1},
-    {id: 4, parentId: 1},
-    {id: 5, parentId: 2},
-    {id: 6, parentId: 4},
-    {id: 7, parentId: 5}
-];
-
-let result = [], idx = {0: {children: result}};
-for (let i = 0; i < nodes2.length; i++) {
-    let node = nodes2[i];
-    let parent = idx[node.parentId];
-    idx[node.id] = parent.children[parent.children.length] = Object.assign({}, node, {children:[]});
-}
-
-console.log('RESULT:', JSON.stringify(result, null, "\t"));
+    return result;
+};
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const expect = require('chai').expect;
-const unflatten = require('../');
+const flat2tree = require('../');
 
 const nodes = [
     {id: 1, parentId: 0},
@@ -14,17 +14,17 @@ const nodes = [
 ];
 
 describe('Test module', () => {
-    const nested = unflatten(nodes);
+    const nested = flat2tree(nodes);
 
-it('Must be instance of Array', () => expect(nested).to.be.instanceof(Array));
+    it('Must be instance of Array', () => expect(nested).to.be.instanceof(Array));
 
-it('Must be an array with size 2', () => expect(nested).to.have.property('length').that.is.an('number').that.equals(2));
+    it('Must be an array with size 2', () => expect(nested).to.have.property('length').that.is.an('number').that.equals(2));
 
-it('Must have all simple properties', () => nested.map(item => expect(item).to.have.all.keys(['id', 'parentId', 'children'])));
+    it('Must have all simple properties', () => nested.map(item => expect(item).to.have.all.keys(['id', 'parentId', 'children'])));
 
-it('Arrays full lengths must be equal', () => {
-    const deepLength = array => array.length + array.map(item => deepLength(item.children)).reduce((sum, curr) => sum + curr, 0);
+    it('Arrays full lengths must be equal', () => {
+        const deepLength = array => array.length + array.map(item => deepLength(item.children)).reduce((sum, curr) => sum + curr, 0);
 
-expect(nodes).to.have.property('length').that.is.an('number').that.equals(deepLength(nested));
-});
+        expect(nodes).to.have.property('length').that.is.an('number').that.equals(deepLength(nested));
+    });
 });
